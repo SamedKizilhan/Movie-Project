568660f8d568d629e6b4f35a631bbbbe19cbc8d3
SamedKizilhan <skizilhan35@gmail.com>
2025-07-07 17:25:43 +0300

    Completed React Movie Tutorial

---
+++ src/services/api.js
@@ -0,0 +1,18 @@
+const API_KEY = "afc3019bd17d2154ff60e0e79d883145"
+const BASE_URL = "https://api.themoviedb.org/3";
+
+export const getPopularMovies = async () => {
+  const response = await fetch(`${BASE_URL}/movie/popular?api_key=${API_KEY}`);
+  const data = await response.json();
+  return data.results;
+};
+
+export const searchMovies = async (query) => {
+  const response = await fetch(
+    `${BASE_URL}/search/movie?api_key=${API_KEY}&query=${encodeURIComponent(
+      query
+    )}`
+  );
+  const data = await response.json();
+  return data.results;
+};
